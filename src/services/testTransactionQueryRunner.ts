1a1fd48839ea32b39fb849bbf23bb4a047e0ac94
liyongal <liyongal@enn.cn>
2022-03-06 11:16:55 +0800

    服务调用处理单元传入参取结果范例

--- src/services/testTransactionQueryRunner.ts
+++ src/services/testTransactionQueryRunner.ts
@@ -6,7 +6,7 @@ import { User } from "src/entity/User";
  * 可以考虑设计注解，但是并没有什么特别的好处。
  * 对于参与事务的处理单元，统一使用 getConnFromThread(name) 获取数据库连接
  */
-export async function testTransactionQueryRunner() {
+export async function testTransactionQueryRunner(id: number) {
   // 在 async thread 开始时自动进行
   const queryRunner = await getConnFromThread('postgis');
   const manager = queryRunner.manager;
@@ -22,14 +22,17 @@ export async function testTransactionQueryRunner() {
 
   // 在 async thread 结束时自动执行
   // await queryRunner.commitTransaction();
+  return { id }
 }
 
 
 /** 模拟一个对外部的服务，需要注册到服务清单，来方便将请求映射到服务 */
 export async function service() {
-  await testTransactionQueryRunner();
-  await testTransactionQueryRunner();
+  const list = [];
+  list.push(await testTransactionQueryRunner(1));
+  list.push(await testTransactionQueryRunner(2));
   return {
     code: 0,
+    data: list,
   }
 }
