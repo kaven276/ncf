7c4ed81f1f4b1bedbcfc191b3179d91bdcd6ce9b
liyongal <liyongal@enn.cn>
2022-03-13 13:46:08 +0800

    增加使用 pg Client,Pool 查询的服务范例

---
+++ packages/examples/src/services/pg/usePool.ts
@@ -0,0 +1,18 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: 'echarts',
+  host: "127.0.0.1",
+  password: "echarts",
+  port: 25432,
+})
+
+interface IResult {
+  /** 当前数据库时间 */
+  now: string,
+}
+/** 测试直接使用 pg 单个连接提供服务 */
+export async function faas() {
+  const res = await pool.query<IResult>('SELECT  NOW()');
+  return res.rows[0].now;
+}
