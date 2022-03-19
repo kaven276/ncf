e42152aef9cf713c690c837026432e737c36fe81
liyongal <liyongal@enn.cn>
2022-03-19 16:21:22 +0800

    proxy 虚拟 faas 模块一样可以受中间件和配置影响

--- packages/examples/src/services/proxy/index.ts
+++ packages/examples/src/services/proxy/index.ts
@@ -1,4 +1,5 @@
 import { getProxiedPath } from '@ncf/engine';
+import { setRandomLatencyConfig } from '../../middlewares/randomLatency';
 
 /** 目录模块导出 faas 代表该目录路径使用反向代理 */
 export async function faas(req: any) {
@@ -7,4 +8,11 @@ export async function faas(req: any) {
     targetPath,
     req,
   };
+}
+
+/** 说明代理模块也可以受中间及其配置影响，和实体 faas 模块一样 */
+export const config = {
+  ...setRandomLatencyConfig({
+    maxLatencyMs: 5000,
+  }),
 }
