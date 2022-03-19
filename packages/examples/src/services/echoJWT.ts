274afc82edd300b1a14d2c03eac03a5555ea19c0
liyongal <liyongal@enn.cn>
2022-03-19 20:43:45 +0800

    microkernel 移除非核心试验代码

--- packages/examples/src/services/echoJWT.ts
+++ packages/examples/src/services/echoJWT.ts
@@ -1,5 +1,5 @@
 import { getJWT, getJWTStruct } from '@ncf/mw-jwt';
-import { throwServiceError } from '@ncf/engine';
+import { throwServiceError } from '@ncf/microkernel';
 
 /** 测试通过专用 API 二维从 asyncLocalStorage 中拿到 jwt 信息，用户标识等等 */
 export async function faas() {
