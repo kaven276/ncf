6c5d6e0aa1d97a965e48c10ea9335efbc81dbf39
liyongal <liyongal@enn.cn>
2022-03-25 20:39:16 +0800

    business interface add comment from entity
    为了传播到各个使用的地方，包括 faas 请求类型

--- packages/examples/src/entity/virus/People.ts
+++ packages/examples/src/entity/virus/People.ts
@@ -4,28 +4,24 @@ import type { Person } from './types';
 export const People = new EntitySchema<Person>({
   name: "person",
   columns: {
-    /** 18位身份证ID */
     id: {
       type: String,
       length: 18,
       primary: true,
       comment: '18位身份证ID',
     },
-    /** 人员姓名 */
     name: {
       type: String,
       length: 10,
       nullable: false,
       comment: '人员姓名',
     },
-    /** 手机号 */
     mobile: {
       type: String,
       length: 11,
       nullable: false,
       comment: '用于联系的11位手机号码'
     },
-    /** 录入时间 */
     regtime: {
       type: Date,
       createDate: true,
--- packages/examples/src/entity/virus/types.d.ts
+++ packages/examples/src/entity/virus/types.d.ts
@@ -1,7 +1,11 @@
 interface Person {
+  /** 18位身份证ID */
   id: string,
+  /** 人员姓名 */
   name: string,
+  /** 手机号 */
   mobile: string,
+  /** 录入时间 */
   regtime: Date,
 }
 
