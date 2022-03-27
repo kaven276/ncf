b6281703f4c5988860496b807b259f3bdf85324a
liyongal <liyongal@enn.cn>
2022-03-27 10:29:55 +0800

    first createQueryBuilder example

---
+++ packages/examples/src/services/typeorm/hr/queryBuilder.ts
@@ -0,0 +1,17 @@
+import { getDataSource } from '@ncf/baas-typeorm';
+import { User } from "src/entity/User";
+
+interface IRequest {
+  id?: User["id"],
+}
+
+/** raw query 方式查询，simple_array/json 等类型都没有解析，都是字符串，不如使用 find, QueryBuild */
+export const faas = async (req: IRequest) => {
+  // return req;
+  const ds = await getDataSource();
+  const userRepo = ds.getRepository(User);
+  const result = await userRepo.createQueryBuilder('user')
+    .where('user.id = :userId', { userId: req.id })
+    .getMany();
+  return result;
+};
