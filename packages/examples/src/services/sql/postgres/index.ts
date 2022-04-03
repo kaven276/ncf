dafb373fc8eb9db61ecc1b3efb05b3ccbbc462e0
liyongal <liyongal@enn.cn>
2022-04-03 14:40:54 +0800

    sqlib pg 直接从 baas module 获取 pool

--- packages/examples/src/services/sql/postgres/index.ts
+++ packages/examples/src/services/sql/postgres/index.ts
@@ -1,8 +1,8 @@
 import { IConfig } from '@ncf/microkernel';
 import { setHowToGetPgConnection } from '@ncf/loader-sql-pg';
-import { getPool } from '@ncf/baas-pg';
+import pool from 'src/bass/pg/pg1';
 
 export const config: IConfig = {
   ext: '.sql',
-  ...setHowToGetPgConnection(() => getPool()),
+  ...setHowToGetPgConnection(() => pool),
 }
