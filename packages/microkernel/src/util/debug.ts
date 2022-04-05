9db2de31aad424bb261c7dfef327e7ae7821cbea
Li Yong <kaven276@vip.sina.com>
2022-04-05 10:38:30 +0800

    debug windows 路径适配

--- packages/microkernel/src/util/debug.ts
+++ packages/microkernel/src/util/debug.ts
@@ -1,16 +1,18 @@
 import { debug } from 'debug';
 import Module from 'module';
 import { ProjectDir } from './resolve';
+import { sep, basename } from 'path';
 
 const len = ProjectDir.length;
+const BAAS = sep + 'baas' + sep;
 
 /** 自动根据当前的 faas/middleware */
 export function getDebug(m: Module) {
   if (m.path.startsWith(ProjectDir)) {
     const restPath = m.filename.substring(len);
     // console.log('restPath', restPath,  m.filename, m.id);
-    const sects = restPath.split('/');
-    sects[sects.length - 1] = sects[sects.length - 1]
+    const sects = restPath.split(sep);
+    sects[sects.length - 1] = basename(sects[sects.length - 1], '.ts');
     if (sects[1] === 'src') {
       if (sects[2] === 'services') {
         // svc: / 后面带空格，方便 vscode 直接 option click 调转到输出日志的源码文件
@@ -20,17 +22,17 @@ export function getDebug(m: Module) {
       }
     }
   }
-  if (m.filename.includes('/baas/')) {
+  if (m.filename.includes(BAAS)) {
     // console.log('baas path', m.path);
-    return debug('baas: ' + m.filename.split('/baas/').pop()!);
+    return debug('baas: ' + m.filename.split(BAAS).pop()!);
   }
-  const sects = m.filename.split('/src/');
+  const sects = m.filename.split(sep + 'src' + sep);
   if (sects.length > 1) {
-    const packageName = sects[0].split('/').pop();
+    const packageName = sects[0].split(sep).pop();
     const inPkgPath = sects.pop();
     // console.log('pkg: path', m.path)
     return debug(`${packageName}: ${inPkgPath}`);
   }
 
-  return debug(`others: ${m.path.split('/').slice(-3).join('/')}`);
+  return debug(`others: ${m.path.split(sep).slice(-3).join('/')}`);
 }
