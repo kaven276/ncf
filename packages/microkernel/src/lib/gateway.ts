902d952209868f296bfe675ee9d297daa3d91807
Li Yong <kaven276@vip.sina.com>
2022-04-08 17:50:11 +0800

    补上拉下的新建文件 gateway.ts

---
+++ packages/microkernel/src/lib/gateway.ts
@@ -0,0 +1,32 @@
+// 定义各种 gateway 接入类型
+import { IncomingMessage, ServerResponse } from 'http';
+import type { Context } from 'koa';
+
+/**
+ * http: 直接通过 nodejs 内置 http 接入核心
+ * koa: 通过 koa 方式接入核心
+ */
+export type GatewayType = 'http' | 'koa';
+
+
+export interface GwHttp {
+  gwtype: 'http',
+  /** nodejs 原生 http callback 的 req,res */
+  readonly http: {
+    readonly req: IncomingMessage,
+    readonly res: ServerResponse,
+  },
+}
+
+export interface GwKoa {
+  gwtype: 'koa',
+  /** nodejs 原生 http callback 的 req,res */
+  readonly http: {
+    readonly req: IncomingMessage,
+    readonly res: ServerResponse,
+  },
+  /** koa ctx */
+  ctx: Context,
+}
+
+export type GwExtras = GwHttp | GwKoa;
