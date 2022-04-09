1ac19c851f5a3484b7037182d7078176bb467748
Li Yong <kaven276@vip.sina.com>
2022-04-09 20:40:58 +0800

    loader-cfg-yaml 使用resolved方式一次性加载同步方式使用

--- packages/loader-cfg-yaml/index.ts
+++ packages/loader-cfg-yaml/index.ts
@@ -1,23 +1,21 @@
 import { Module } from 'module';
 import { readFile } from 'fs/promises';
 import { load } from "js-yaml";
+import { resolved, throwServiceError } from '@ncf/microkernel';
 
 // loader 是异步的，promise 返回结果
 // 这样一些非常大的文件，可以被 loader 分片处理，防止独占时间循环造成主线程卡死
 
 //@ts-ignore
 Module._extensions['.yaml'] = Module._extensions['.yml'] = function loaderYAML(m: any, filename: string) {
-  const contentPromise = readFile(filename, { 'encoding': 'utf8' }).then(txt => load(txt, { json: true }))
-    .catch(e => ({ error: e }));
   m.exports = {
+    content: resolved(() => readFile(filename, { 'encoding': 'utf8' })
+      .then(txt => load(txt, { json: true }))
+      .catch(e => ({ error: e }))),
     faas: async () => {
-      const result: any = await contentPromise;
+      const result: any = m.exports.content;
       if (result.error) {
-        throw {
-          code: 1,
-          msg: 'loader-cfg-yaml convert failed',
-          data: result.error,
-        }
+        throwServiceError(1, 'loader-cfg-yaml convert failed', result.error);
       }
       return result;
     },
