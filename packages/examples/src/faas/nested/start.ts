45c2fcd538a99e85a4ffdae6fecadd8484e58011
Li Yong <kaven276@vip.sina.com>
2022-04-17 22:39:33 +0800

    cache 中间件支持配置有效期，保存快照到缓存

--- packages/examples/src/faas/nested/start.ts
+++ packages/examples/src/faas/nested/start.ts
@@ -1,12 +1,21 @@
 import { level1 } from './level1';
 import { state2 } from './state2';
+import { setCacheConfig } from 'src/mw/cache';
 
 export function getCacheKey(req: any): string {
   return String(req?.multi ?? 2);
 }
 
+export const config = {
+  ...setCacheConfig({
+    /** 缓存有效期 10s */
+    maxAge: 10 * 1000,
+  })
+}
+
 /** 查看 level2 ts 改变，是否再访问 start，看到是更新后的代码 */
 export async function faas(req: any) {
+  console.log('exec start')
   await new Promise(r => setTimeout(r, Math.random() * 3000));
   // console.log(state2);
   return {
--- packages/examples/src/mw/cache/index.ts
+++ packages/examples/src/mw/cache/index.ts
@@ -11,18 +11,20 @@ declare module '@ncf/microkernel' {
 }
 
 export interface CacheConfig {
-  /** 缓存最大有效的毫秒数 */  maxAge: number,
+  /** 缓存最大有效的毫秒数 */
+  maxAge: number,
 }
 
 const configKey = Symbol('Cache');
 
 const defaultConfig: CacheConfig = {
-  maxAge: 3000,
+  /** 默认1分钟缓存有效期 */
+  maxAge: 60 * 1000,
 }
 
 interface CachedItem {
   content: any,
-  createdAt: Date,
+  createdAt: number,
   hitCount: number,
 }
 type OneFaasCache = Map<string, CachedItem>;
@@ -43,19 +45,25 @@ export const mwCache: IMiddleWare = async (ctx, next) => {
       }
       let cached = faasCache.get(cacheKey);
       if (cached) {
-        cached.hitCount += 1;
-        ctx.response = cached.content;
-        debug(`${ctx.path}, ${cacheKey} cache hit ${cached.hitCount} times`);
-        return;
+        if (Date.now() - cached.createdAt < config.maxAge) {
+          cached.hitCount += 1;
+          ctx.response = cached.content;
+          debug(`${ctx.path}, ${cacheKey} cache hit ${cached.hitCount} times`);
+          return;
+        } else {
+          debug(`${ctx.path}, ${cacheKey} cache over maxAge`);
+        }
       } else {
-        await next();
-        cached = {
-          content: ctx.response,
-          createdAt: new Date(),
-          hitCount: 0,
-        };
-        faasCache.set(cacheKey, cached);
+        debug(`${ctx.path}, ${cacheKey} cache miss`);
       }
+      await next();
+      cached = {
+        content: ctx.response,
+        createdAt: Date.now(),
+        hitCount: cached ? cached.hitCount : 0,
+      };
+      debug(`${ctx.path}, ${cacheKey} cache save`);
+      faasCache.set(cacheKey, JSON.parse(JSON.stringify(cached)));
     }
   } else {
     await next();
