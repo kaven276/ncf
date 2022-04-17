855f887915ba9ded613714594729c1f93f3449f9
Li Yong <kaven276@vip.sina.com>
2022-04-17 14:58:39 +0800

    microkernel bugfix Boolean(...)

--- packages/microkernel/src/util/resolve.ts
+++ packages/microkernel/src/util/resolve.ts
@@ -3,6 +3,6 @@ import { Module } from 'module';
 export const ProjectDir = process.cwd();
 
 //@ts-ignore
-const tsMode = Booean(Module._extensions['.ts']);
+const tsMode = Boolean(Module._extensions['.ts']);
 export const jsExt = tsMode ? '.ts' : '.js';
 export const MoundDir = tsMode ? 'src' : 'dist';
