73b8e37e2fea9e03bbc66a733cc9c4decdd4ca8d
Li Yong <kaven276@vip.sina.com>
2022-04-20 16:52:10 +0800

    getDebug key 优化

--- packages/microkernel/src/util/debug.ts
+++ packages/microkernel/src/util/debug.ts
@@ -4,7 +4,8 @@ import { ProjectDir, jsExt, MoundDir } from './resolve';
 import { sep, basename } from 'path';
 
 const len = ProjectDir.length;
-const BAAS = sep + 'baas' + sep;
+
+const noop = () => { };
 
 /** 自动根据当前的 faas/middleware */
 export function getDebug(m: Module) {
@@ -14,25 +15,30 @@ export function getDebug(m: Module) {
     const sects = restPath.split(sep);
     sects[sects.length - 1] = basename(sects[sects.length - 1], jsExt);
     if (sects[1] === MoundDir) {
-      if (sects[2] === 'faas') {
-        // svc: / 后面带空格，方便 vscode 直接 option click 调转到输出日志的源码文件
-        return debug(`svc: ${sects.slice(3).join('/')}`);
-      } else if (sects[2] === 'middlewares') {
-        return debug(`mw: ${sects.slice(3).join('/')}`);
-      }
+      // svc: / 后面带空格，方便 vscode 直接 option click 调转到输出日志的源码文件
+      return debug(`${sects[2]}:${sects.slice(3).join('/')}`);
     }
   }
-  if (m.filename.includes(BAAS)) {
-    // console.log('baas path', m.path);
-    return debug('baas: ' + m.filename.split(BAAS).pop()!);
-  }
+
   const sects = m.filename.split(sep + MoundDir + sep);
   if (sects.length > 1) {
     const packageName = sects[0].split(sep).pop();
     const inPkgPath = sects.pop();
     // console.log('pkg: path', m.path)
-    return debug(`${packageName}: ${inPkgPath}`);
+    return debug(`${packageName}:${inPkgPath}`);
   }
 
-  return debug(`others: ${m.path.split(sep).slice(-3).join('/')}`);
+  if (m.path.includes('node_modules')) {
+    return noop;
+  }
+
+  // 在monorepo中，和应用在同级的其他包
+  if (m.path.includes('packages')) {
+    const relative = m.filename.split(sep + 'packages' + sep)[1].split(sep);
+    const pkgName = relative[0];
+    const fileName = relative.pop();
+    return debug(`${pkgName}:${fileName}`);
+  }
+
+  return debug(`others:${m.path.split(sep).slice(-3).join('/')}`);
 }
