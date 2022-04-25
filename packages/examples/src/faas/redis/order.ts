21da9443309faeeaf5b41bcfb1856ad93c210de2
Li Yong <kaven276@vip.sina.com>
2022-04-25 15:02:55 +0800

    zset 范例可以看到创建的订单号，方便和排位比较

--- packages/examples/src/faas/redis/order.ts
+++ packages/examples/src/faas/redis/order.ts
@@ -6,24 +6,29 @@ interface Api {
   request: {
     /** 大家竞争订单的区域 */
     area?: string,
+    /** 用户标识 */
+    userid: string,
   },
   response: {
+    /** 生成的订单标识 */
+    orderId: number,
     /** 在同一个 area，本订单排在第几位 */
-    'order': number,
+    order: number,
   }
 }
 
 const OrderKey = 'Order';
-let orderId: number = 0;
+let seqOrderId: number = 0;
 
 export const faas: Service<Api> = async (req) => {
+  const orderId = ++seqOrderId;
   const key = `${OrderKey}:${req.area ?? 'default'}`;
   const now = Date.now();
-  await redis.zadd(key, ++orderId, now);
+  await redis.zadd(key, orderId, now);
   const order = (await redis.zrank(key, now))!;
   // 默认一些订单已经被完成
   setTimeout(() => {
     redis.zrem(key, now);
   }, (Math.random() * 5 + 0) * 1000);
-  return { order }
+  return { orderId, order }
 }
