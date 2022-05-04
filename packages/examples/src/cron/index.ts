2a60d1d2686af5549c7bf41e1502f7f895cafb8a
Li Yong <kaven276@vip.sina.com>
2022-05-04 19:05:10 +0800

    直接用 node-schedule 自带的全部关闭功能支持优雅退出

--- packages/examples/src/cron/index.ts
+++ packages/examples/src/cron/index.ts
@@ -1,3 +1,7 @@
 // import 各个任务定义文件，自动触发 schedule 来定时执行所有任务
-
+import { gracefulShutdown } from 'node-schedule';
 import './task1';
+
+process.on('SIGINT', function () {
+  gracefulShutdown().then(() => console.log('gracefule shutdown all jobs'));
+});
--- packages/examples/src/cron/task1.ts
+++ packages/examples/src/cron/task1.ts
@@ -1,4 +1,4 @@
-import { outerCall, addDisposer } from '@ncf/microkernel';
+import { outerCall } from '@ncf/microkernel';
 import { scheduleJob } from 'node-schedule';
 import { ISpec } from 'src/faas/later/order.spec';
 
@@ -10,5 +10,5 @@ export const job = scheduleJob('fake orders', '*/5 * * * * *', () => {
   });
 });
 
-// 以后可能提供自动扫描功能，对 export job 的模块自动添加安全 cancel，减少模板代码书写
-addDisposer(() => job.cancel());
+// 查看下次待执行时间，检查是否是在本时区计算的
+// console.log(job.nextInvocation());
