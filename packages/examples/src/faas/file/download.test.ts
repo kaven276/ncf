ce91298779f9c926b480b87a77a3ed65c1c4cf21
Li Yong <kaven276@vip.sina.com>
2022-05-14 21:57:00 +0800

    download.test.ts 增加 tap 测试

--- packages/examples/src/faas/file/download.test.ts
+++ packages/examples/src/faas/file/download.test.ts
@@ -3,3 +3,9 @@ import { outerCall } from '@ncf/microkernel';
 export const faas = async () => {
   return outerCall('/file/download', { filename: '奖状.jpg'});
 }
+
+import tap from 'tap';
+/** 如果是顶层模块，认为是要测试任务 */
+export const task = (require.main === module) && tap.test('order', async (t) => {
+  await faas();
+});
