6492ad5ae406e8ab4654dc1a34ca55c3fee234ff
Li Yong <kaven276@vip.sina.com>
2022-05-14 21:36:47 +0800

    已有 .test.ts 增加 tap 测试

--- packages/examples/src/faas/esm/getUrl.test.ts
+++ packages/examples/src/faas/esm/getUrl.test.ts
@@ -3,3 +3,9 @@ import { innerCall } from '@ncf/microkernel';
 export const faas = async () => {
   return innerCall('/esm/getUrl', {});
 }
+
+import tap from 'tap';
+/** 如果是顶层模块，认为是要测试任务 */
+export const task = (require.main === module) && tap.test('getUrl', async (t) => {
+  await faas();
+});
--- packages/examples/src/faas/esm/useNcfFetch.test.ts
+++ packages/examples/src/faas/esm/useNcfFetch.test.ts
@@ -3,3 +3,9 @@ import { innerCall } from '@ncf/microkernel';
 export const faas = async () => {
   return innerCall('/esm/useNcfFetch', {});
 }
+
+import tap from 'tap';
+/** 如果是顶层模块，认为是要测试任务 */
+export const task = (require.main === module) && tap.test('useNcfFetch', async (t) => {
+  await faas();
+});
