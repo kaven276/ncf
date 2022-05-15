ec9a794d0d0328f3b37dd7ccb76f6bf9e381dcf6
Li Yong <kaven276@vip.sina.com>
2022-05-15 09:19:39 +0800

    redis baas 配置连接断开重试策略

--- packages/examples/src/baas/ioredis/redisdb1.baas.ts
+++ packages/examples/src/baas/ioredis/redisdb1.baas.ts
@@ -1,6 +1,13 @@
 import { makeRedisClient } from '@ncf/ioredis';
 import { env } from 'src/env';
 
-let redisClient = makeRedisClient(env.REDIS_URL);
+let redisClient = makeRedisClient(env.REDIS_URL, {
+  // This is the default value of `retryStrategy`
+  retryStrategy(times) {
+    const delay = Math.min(Math.pow(1.5, times) * 50, 10000);
+    console.log(`redis connect retry ${times}times, will wait for ${delay}ms`);
+    return delay;
+  },
+});
 
 export default redisClient;
