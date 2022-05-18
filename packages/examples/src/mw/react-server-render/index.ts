4d5aead3c135fd7bdcbf0b5786152bb9771bbf58
Li Yong <kaven276@vip.sina.com>
2022-05-18 09:07:57 +0800

    fix react-server-render 中间件函数说明

--- packages/examples/src/mw/react-server-render/index.ts
+++ packages/examples/src/mw/react-server-render/index.ts
@@ -1,7 +1,7 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 import { IMiddleWare } from '@ncf/microkernel';
 
-/** 延迟开始执行不超过任意毫秒数  */
+/** 如果响应是 react 元素，则渲染成 html 字符串，返回转换完的响应  */
 export const mwReactServerRender: IMiddleWare = async (ctx, next) => {
   await next();
 
