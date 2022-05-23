d570cec74ed7bdbec671e826e68dccd074218e38
Li Yong <kaven276@vip.sina.com>
2022-05-23 08:03:33 +0800

    增加了 mysql 范例，复用了 sqlite 范例建模和服务

---
+++ packages/examples/src/baas/mysql/mysql.baas.ts
@@ -0,0 +1,20 @@
+import { createDataSource } from '@ncf/typeorm';
+import { pathPattern } from '@ncf/microkernel';
+
+/** sqlite 的数据源对象 */
+let baas = createDataSource({
+  type: "mysql",
+  host: "127.0.0.1",
+  port: 3306,
+  database: 'wordpress',
+  username: 'root',
+  password: '123456',
+  synchronize: true,
+  logging: true,
+  // 复用一下 sqlite 的模型
+  entities: [pathPattern("baas/sqlite/entity/**/*")],
+  migrations: [pathPattern("baas/sqlite/migration/**/*")],
+  subscribers: [pathPattern("baas/sqlite/subscriber/**/*")],
+});
+
+export default baas;
--- packages/examples/src/faas/typeorm/sqlite/index.ts
+++ packages/examples/src/faas/typeorm/sqlite/index.ts
@@ -1,1 +1,2 @@
 export { default } from 'src/baas/sqlite/sqlite.baas';
+// export { default } from 'src/baas/mysql/mysql.baas';
