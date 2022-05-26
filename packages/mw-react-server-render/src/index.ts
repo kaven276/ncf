a2782ed48332d3287bfe74bf45ead895352ac998
Li Yong <kaven276@vip.sina.com>
2022-05-26 23:03:11 +0800

    react服务渲染中间件支持Layout/title配置，添加范例

--- packages/mw-react-server-render/src/index.ts
+++ packages/mw-react-server-render/src/index.ts
@@ -1,11 +1,38 @@
 import { renderToStaticMarkup } from 'react-dom/server';
-import { IMiddleWare } from '@ncf/microkernel';
+import { IMiddleWare, getConfig } from '@ncf/microkernel';
+import type { ComponentType } from 'react';
+import { createElement } from 'react';
+
+const configKey = Symbol('layout');
+
+export interface ReactServerRenderConfig {
+  /** Layout 模板指定 */
+  Layout?: ComponentType,
+  /** 页面标题 */
+  title?: string,
+}
+
+const defaultConfig: ReactServerRenderConfig = {
+}
+
+export function setReactServerRenderConfig(cfg: ReactServerRenderConfig) {
+  return {
+    [configKey!]: cfg
+  }
+}
 
 /** 如果响应是 react 元素，则渲染成 html 字符串，返回转换完的响应  */
 export const mwReactServerRender: IMiddleWare = async (ctx, next) => {
   await next();
 
   if ((typeof ctx.response === 'object') && (typeof ctx.response['$$typeof'] === 'symbol')) {
-    ctx.response = renderToStaticMarkup(ctx.response)
+    const config: ReactServerRenderConfig = getConfig(configKey, ctx) || defaultConfig;
+    if (config.Layout) {
+      // 如果配置了 layout 则使用 layout
+      //@ts-ignore
+      ctx.response = renderToStaticMarkup(createElement(config.Layout, { head: { title: config.title } }, ctx.response));
+    } else {
+      ctx.response = renderToStaticMarkup(ctx.response);
+    }
   }
 }
