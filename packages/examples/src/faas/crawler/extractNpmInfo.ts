8b28006ab1e89c525401eee16744daf6f2b4f00b
Li Yong <kaven276@vip.sina.com>
2022-05-28 07:38:44 +0800

    爬虫范例支持参数指定npm包名，可抓取指定包 meta info

--- packages/examples/src/faas/crawler/extractNpmInfo.ts
+++ packages/examples/src/faas/crawler/extractNpmInfo.ts
@@ -1,12 +1,18 @@
 import * as cheerio from 'cheerio';
 
-const URL = 'https://www.npmjs.com/package/cheerio';
+const URL = 'https://www.npmjs.com/package/';
 
-export const faas = async () => {
-  const htmlText = await fetch(URL).then(resp => resp.text());
+interface Request {
+  pkgName: string,
+}
+
+/** 搜索指定 npm 包的 npm 条目网页，提取其中的 meta 信息 */
+export const faas = async (req: Request) => {
+  req.pkgName = req.pkgName ?? 'lodash';
+  const htmlText = await fetch(URL + req.pkgName).then(resp => resp.text());
   const $ = cheerio.load(htmlText);
   const downloads = $('p.f4.tl.flex-auto.fw6.black-80.ma0.pr2.pb1').text();
   const version = $('p.f2874b88.fw6.mb3.mt2.truncate.black-80.f4').first().text();
   const license = $('p.f2874b88.fw6.mb3.mt2.truncate.black-80.f4').eq(1).text();
-  return { downloads, version, license };
+  return { package: req.pkgName, downloads, version, license };
 }
