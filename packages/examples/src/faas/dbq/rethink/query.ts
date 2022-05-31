89e23a33032ffcb80e30b0d2bfebc1d47e69cd93
Li Yong <kaven276@vip.sina.com>
2022-05-31 22:07:52 +0800

    rethink insert/query 剥离查询，每次直接用提前构建好的

--- packages/examples/src/faas/dbq/rethink/query.ts
+++ packages/examples/src/faas/dbq/rethink/query.ts
@@ -1,6 +1,8 @@
 import connection from '.';
 import r from 'rethinkdb';
 
+const reql = r.table('authors').filter(r.row('name').eq("William Adama"));
+
 export const faas = async () => {
-  return r.table('authors').filter(r.row('name').eq("William Adama")).run(connection);
+  return reql.run(connection);
 }
