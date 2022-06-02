d45a36d6093307acc5a1fae20a89c3902393fdd6
Li Yong <kaven276@vip.sina.com>
2022-06-02 08:11:47 +0800

    longrunning 使用 node:timers/promises 简化

--- packages/examples/src/faas/longrunning.ts
+++ packages/examples/src/faas/longrunning.ts
@@ -1,5 +1,7 @@
+import { setTimeout } from 'node:timers/promises';
+
 /** 测试一个长任务是否可以 survive SIGHUP，gracefule quit */
 export const faas = async () => {
-  await new Promise(r => setTimeout(r, 10 * 1000));
+  await setTimeout(10 * 1000);
   return 'sorry let you wait too long';
 }
