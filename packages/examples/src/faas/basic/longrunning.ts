0abd50a9f00aa20030063153b596004f82eecb07
Li Yong <kaven276@vip.sina.com>
2022-06-05 20:13:36 +0800

    basic/longRunning 范例添加 cache 策略

--- packages/examples/src/faas/basic/longrunning.ts
+++ packages/examples/src/faas/basic/longrunning.ts
@@ -1,4 +1,14 @@
 import { setTimeout } from 'node:timers/promises';
+import { cfgCache, cfgCacheKeyFn } from 'src/mw/cache';
+
+// 对于一个执行耗时的服务，推荐配置缓存策略，降低服务端压力
+export const config = {
+  ...cfgCache.set({
+    /** 缓存有效期 10s */
+    maxAge: 10 * 1000,
+  }),
+  ...cfgCacheKeyFn.set(() => 'fixed')
+}
 
 /** 测试一个长任务是否可以 survive SIGHUP，gracefule quit */
 export const faas = async () => {
