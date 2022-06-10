899009821a50cc3b731ad6729283efc4854737c8
Li Yong <kaven276@vip.sina.com>
2022-06-10 16:15:58 +0800

    resolve ServiceDir, pathPattern 考虑 windows

--- packages/microkernel/src/util/resolve.ts
+++ packages/microkernel/src/util/resolve.ts
@@ -7,12 +7,12 @@ export const ProjectDir = process.cwd();
 export const tsMode = Boolean(Module._extensions['.ts']);
 export const jsExt = tsMode ? '.ts' : '.js';
 export const MoundDir = tsMode ? 'src' : 'dist';
-export const ServiceDir = `${ProjectDir}/${MoundDir}/faas`;
+export const ServiceDir = normalize(`${ProjectDir}/${MoundDir}/faas`);
 export const prefixLength = ServiceDir.length;
 
 /** 将路径按照 js/ts 运行时调整到匹配的完整路径 */
 export function pathPattern(fpath: string) {
-  return tsMode ? `src/${fpath}.ts` : `dist/${fpath}.js`;
+  return normalize(tsMode ? `src/${fpath}.ts` : `dist/${fpath}.js`);
 }
 
 export const MiddlewareFilePath = normalize(`${ProjectDir}/${MoundDir}/middlewares${jsExt}`);
