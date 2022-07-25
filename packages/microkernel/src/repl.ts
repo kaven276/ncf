2e8494104c1772a97366192554141163b3f13881
Li Yong <kaven276@vip.sina.com>
2022-07-25 12:49:33 +0800

    kernel REPL 只区分是否返回 undefined，返回0也能输出了

--- packages/microkernel/src/repl.ts
+++ packages/microkernel/src/repl.ts
@@ -64,10 +64,10 @@ async function doTest() {
       const respPath = lastModifiedFaasModulePath.replace(jsExt, '.resp.jpg');
       const f = createWriteStream(respPath);
       resp.pipe(f);
-    } else if (!!resp) {
+    } else {
       const respPath = lastModifiedFaasModulePath.replace(jsExt, '.resp.json');
       console.log(respPath);
-      writeFile(respPath, JSON.stringify(resp, null, 2), { encoding: 'utf8' }, () => { });
+      writeFile(respPath, resp === undefined ? '' : JSON.stringify(resp, null, 2), { encoding: 'utf8' }, () => { });
     }
   }
 }
