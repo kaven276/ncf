1fe7bd80b0c1153d1ec4ac72519173b672d60611
Li Yong <kaven276@vip.sina.com>
2022-07-26 10:34:51 +0800

    范例 queryBuilder 增加关联表并在上面添加 where 条件

--- packages/examples/src/faas/typeorm/hr/queryBuilder.ts
+++ packages/examples/src/faas/typeorm/hr/queryBuilder.ts
@@ -1,16 +1,34 @@
 import ds from '.';
 import { User } from "entity/User";
+import { Org } from "entity/Org";
+import { Service } from '@ncf/microkernel';
 
 interface IRequest {
   id?: User["id"],
+  rank: number,
+}
+
+interface API {
+  path: '/typeorm/hr/queryBuilder',
+  request: IRequest,
+  response: User[],
 }
 
 /** raw query 方式查询，simple_array/json 等类型都没有解析，都是字符串，不如使用 find, QueryBuild */
-export const faas = async (req: IRequest) => {
+export const faas: Service<API> = async (req: IRequest) => {
   // return req;
   const userRepo = ds.getRepository(User);
   const result = await userRepo.createQueryBuilder('user')
+    .leftJoinAndSelect("user.org", "org")
     .where('user.id = :userId', { userId: req.id })
+    .andWhere('org."rank" = :rank', { rank: req.rank })
     .getMany();
   return result;
 };
+
+faas.tests = {
+  test1: {
+    id: 2,
+    rank: 1,
+  }
+}
